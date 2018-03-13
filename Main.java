13170164d0c9fd6fd9f77d8965300f88dbb6d942
walker.sakatch <walker.sakatch@zone46-wa.cs.ucalgary.ca>
2018-03-13 13:05:53 -0600

    Store contents of file into a String

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 import org.eclipse.jdt.core.*;
@@ -11,24 +15,37 @@ public class Main {
 		
 		//System.out.print("Please enter the file path: ");
 		//String baseDir = input.nextLine();
-		String baseDir = "/home/ugb/walker.sakatch/Desktop/SENG 300/GRPAssign1";
+		String baseDir = "/home/ugb/walker.sakatch/Desktop/SENG 300/GRPAssign1/";
 		File folder = new File(baseDir);
 		File[] files = folder.listFiles();
 		
 		for(int i = 0; i < files.length; i++) {
-			/*
+			
 			if(files[i].getName().endsWith(".java")) {
-				System.out.println(files[i].getName());
+				String fileName = files[i].getName();
+				String path = baseDir + fileName;
+				String content = null;
+				System.out.println(fileName);
+				
+				try {
+					byte[] encoded = Files.readAllBytes(Paths.get(path));
+					content = new String(encoded);
+				} catch (IOException e) {
+					System.out.println("An error occured accessing:" + fileName);
+				}
+				
+				System.out.print(content);
 			}
 			
 			
-			if(files[i].getName().endsWith(".java")) {
+			/*if(files[i].getName().endsWith(".java")) {
+			
 				parse(files[i]);
-			}
+			}*/
 			
-			*/
+			
 
-			parse("public class Bar {}");
+			//parse("public class Bar {}");
 		}
 	}
 	
